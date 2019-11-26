7007bce5983348ee4e7c8f83535f1ce243fa476e
Mattheeeew <44161476+Mattheeeew@users.noreply.github.com>
2019-11-26 23:54:52 +0100

    Added User Control to check Digital Signature.

---
+++ CryptographyDigitalSignature/CryptographyDigitalSignature/CheckDigitalSignature.Designer.cs
@@ -0,0 +1,194 @@
+﻿namespace CryptographyDigitalSignature
+{
+    partial class CheckDigitalSignatureUserControl
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnCheck = new MetroSet_UI.Controls.MetroSetButton();
+            this.btnChooseFile = new MetroSet_UI.Controls.MetroSetButton();
+            this.btnChooseDigitalSignature = new MetroSet_UI.Controls.MetroSetButton();
+            this.tbxPlainTekst = new MetroSet_UI.Controls.MetroSetTextBox();
+            this.metroSetLabel3 = new MetroSet_UI.Controls.MetroSetLabel();
+            this.metroSetLabel1 = new MetroSet_UI.Controls.MetroSetLabel();
+            this.SuspendLayout();
+            // 
+            // btnCheck
+            // 
+            this.btnCheck.DisabledBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(120)))), ((int)(((byte)(65)))), ((int)(((byte)(177)))), ((int)(((byte)(225)))));
+            this.btnCheck.DisabledBorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(120)))), ((int)(((byte)(65)))), ((int)(((byte)(177)))), ((int)(((byte)(225)))));
+            this.btnCheck.DisabledForeColor = System.Drawing.Color.Gray;
+            this.btnCheck.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCheck.HoverBorderColor = System.Drawing.Color.Maroon;
+            this.btnCheck.HoverColor = System.Drawing.Color.Maroon;
+            this.btnCheck.HoverTextColor = System.Drawing.Color.White;
+            this.btnCheck.Location = new System.Drawing.Point(35, 365);
+            this.btnCheck.Name = "btnCheck";
+            this.btnCheck.NormalBorderColor = System.Drawing.Color.Maroon;
+            this.btnCheck.NormalColor = System.Drawing.Color.Maroon;
+            this.btnCheck.NormalTextColor = System.Drawing.Color.White;
+            this.btnCheck.PressBorderColor = System.Drawing.Color.Maroon;
+            this.btnCheck.PressColor = System.Drawing.Color.Maroon;
+            this.btnCheck.PressTextColor = System.Drawing.Color.White;
+            this.btnCheck.Size = new System.Drawing.Size(155, 31);
+            this.btnCheck.Style = MetroSet_UI.Design.Style.Custom;
+            this.btnCheck.StyleManager = null;
+            this.btnCheck.TabIndex = 24;
+            this.btnCheck.Text = "Check";
+            this.btnCheck.ThemeAuthor = "Narwin";
+            this.btnCheck.ThemeName = "MetroLite";
+            // 
+            // btnChooseFile
+            // 
+            this.btnChooseFile.DisabledBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(120)))), ((int)(((byte)(65)))), ((int)(((byte)(177)))), ((int)(((byte)(225)))));
+            this.btnChooseFile.DisabledBorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(120)))), ((int)(((byte)(65)))), ((int)(((byte)(177)))), ((int)(((byte)(225)))));
+            this.btnChooseFile.DisabledForeColor = System.Drawing.Color.Gray;
+            this.btnChooseFile.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnChooseFile.HoverBorderColor = System.Drawing.Color.Maroon;
+            this.btnChooseFile.HoverColor = System.Drawing.Color.Maroon;
+            this.btnChooseFile.HoverTextColor = System.Drawing.Color.White;
+            this.btnChooseFile.Location = new System.Drawing.Point(35, 253);
+            this.btnChooseFile.Name = "btnChooseFile";
+            this.btnChooseFile.NormalBorderColor = System.Drawing.Color.Maroon;
+            this.btnChooseFile.NormalColor = System.Drawing.Color.Maroon;
+            this.btnChooseFile.NormalTextColor = System.Drawing.Color.White;
+            this.btnChooseFile.PressBorderColor = System.Drawing.Color.Maroon;
+            this.btnChooseFile.PressColor = System.Drawing.Color.Maroon;
+            this.btnChooseFile.PressTextColor = System.Drawing.Color.White;
+            this.btnChooseFile.Size = new System.Drawing.Size(155, 31);
+            this.btnChooseFile.Style = MetroSet_UI.Design.Style.Custom;
+            this.btnChooseFile.StyleManager = null;
+            this.btnChooseFile.TabIndex = 23;
+            this.btnChooseFile.Text = "Choose File";
+            this.btnChooseFile.ThemeAuthor = "Narwin";
+            this.btnChooseFile.ThemeName = "MetroLite";
+            // 
+            // btnChooseDigitalSignature
+            // 
+            this.btnChooseDigitalSignature.DisabledBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(120)))), ((int)(((byte)(65)))), ((int)(((byte)(177)))), ((int)(((byte)(225)))));
+            this.btnChooseDigitalSignature.DisabledBorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(120)))), ((int)(((byte)(65)))), ((int)(((byte)(177)))), ((int)(((byte)(225)))));
+            this.btnChooseDigitalSignature.DisabledForeColor = System.Drawing.Color.Gray;
+            this.btnChooseDigitalSignature.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnChooseDigitalSignature.HoverBorderColor = System.Drawing.Color.Maroon;
+            this.btnChooseDigitalSignature.HoverColor = System.Drawing.Color.Maroon;
+            this.btnChooseDigitalSignature.HoverTextColor = System.Drawing.Color.White;
+            this.btnChooseDigitalSignature.Location = new System.Drawing.Point(35, 290);
+            this.btnChooseDigitalSignature.Name = "btnChooseDigitalSignature";
+            this.btnChooseDigitalSignature.NormalBorderColor = System.Drawing.Color.Maroon;
+            this.btnChooseDigitalSignature.NormalColor = System.Drawing.Color.Maroon;
+            this.btnChooseDigitalSignature.NormalTextColor = System.Drawing.Color.White;
+            this.btnChooseDigitalSignature.PressBorderColor = System.Drawing.Color.Maroon;
+            this.btnChooseDigitalSignature.PressColor = System.Drawing.Color.Maroon;
+            this.btnChooseDigitalSignature.PressTextColor = System.Drawing.Color.White;
+            this.btnChooseDigitalSignature.Size = new System.Drawing.Size(155, 31);
+            this.btnChooseDigitalSignature.Style = MetroSet_UI.Design.Style.Custom;
+            this.btnChooseDigitalSignature.StyleManager = null;
+            this.btnChooseDigitalSignature.TabIndex = 22;
+            this.btnChooseDigitalSignature.Text = "Choose Digital Signature";
+            this.btnChooseDigitalSignature.ThemeAuthor = "Narwin";
+            this.btnChooseDigitalSignature.ThemeName = "MetroLite";
+            // 
+            // tbxPlainTekst
+            // 
+            this.tbxPlainTekst.AutoCompleteCustomSource = null;
+            this.tbxPlainTekst.AutoCompleteMode = System.Windows.Forms.AutoCompleteMode.None;
+            this.tbxPlainTekst.AutoCompleteSource = System.Windows.Forms.AutoCompleteSource.None;
+            this.tbxPlainTekst.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(155)))), ((int)(((byte)(155)))), ((int)(((byte)(155)))));
+            this.tbxPlainTekst.DisabledBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(204)))), ((int)(((byte)(204)))), ((int)(((byte)(204)))));
+            this.tbxPlainTekst.DisabledBorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(155)))), ((int)(((byte)(155)))), ((int)(((byte)(155)))));
+            this.tbxPlainTekst.DisabledForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(136)))), ((int)(((byte)(136)))), ((int)(((byte)(136)))));
+            this.tbxPlainTekst.Font = new System.Drawing.Font("Segoe UI", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tbxPlainTekst.HoverColor = System.Drawing.Color.FromArgb(((int)(((byte)(102)))), ((int)(((byte)(102)))), ((int)(((byte)(102)))));
+            this.tbxPlainTekst.Image = null;
+            this.tbxPlainTekst.Lines = null;
+            this.tbxPlainTekst.Location = new System.Drawing.Point(35, 77);
+            this.tbxPlainTekst.MaxLength = 32767;
+            this.tbxPlainTekst.Multiline = false;
+            this.tbxPlainTekst.Name = "tbxPlainTekst";
+            this.tbxPlainTekst.ReadOnly = true;
+            this.tbxPlainTekst.Size = new System.Drawing.Size(636, 170);
+            this.tbxPlainTekst.Style = MetroSet_UI.Design.Style.Light;
+            this.tbxPlainTekst.StyleManager = null;
+            this.tbxPlainTekst.TabIndex = 21;
+            this.tbxPlainTekst.TextAlign = System.Windows.Forms.HorizontalAlignment.Left;
+            this.tbxPlainTekst.ThemeAuthor = "Narwin";
+            this.tbxPlainTekst.ThemeName = "MetroLite";
+            this.tbxPlainTekst.UseSystemPasswordChar = false;
+            this.tbxPlainTekst.WatermarkText = "";
+            // 
+            // metroSetLabel3
+            // 
+            this.metroSetLabel3.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.metroSetLabel3.Location = new System.Drawing.Point(43, 34);
+            this.metroSetLabel3.Name = "metroSetLabel3";
+            this.metroSetLabel3.Size = new System.Drawing.Size(365, 28);
+            this.metroSetLabel3.Style = MetroSet_UI.Design.Style.Light;
+            this.metroSetLabel3.StyleManager = null;
+            this.metroSetLabel3.TabIndex = 20;
+            this.metroSetLabel3.Text = "Check data integrity";
+            this.metroSetLabel3.ThemeAuthor = "Narwin";
+            this.metroSetLabel3.ThemeName = "MetroLite";
+            // 
+            // metroSetLabel1
+            // 
+            this.metroSetLabel1.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.metroSetLabel1.Location = new System.Drawing.Point(43, 6);
+            this.metroSetLabel1.Name = "metroSetLabel1";
+            this.metroSetLabel1.Size = new System.Drawing.Size(397, 28);
+            this.metroSetLabel1.Style = MetroSet_UI.Design.Style.Light;
+            this.metroSetLabel1.StyleManager = null;
+            this.metroSetLabel1.TabIndex = 19;
+            this.metroSetLabel1.Text = "Choose file and digital signature";
+            this.metroSetLabel1.ThemeAuthor = "Narwin";
+            this.metroSetLabel1.ThemeName = "MetroLite";
+            // 
+            // CheckDigitalSignatureUserControl
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(96F, 96F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi;
+            this.BackColor = System.Drawing.Color.White;
+            this.Controls.Add(this.btnCheck);
+            this.Controls.Add(this.btnChooseFile);
+            this.Controls.Add(this.btnChooseDigitalSignature);
+            this.Controls.Add(this.tbxPlainTekst);
+            this.Controls.Add(this.metroSetLabel3);
+            this.Controls.Add(this.metroSetLabel1);
+            this.Name = "CheckDigitalSignatureUserControl";
+            this.Size = new System.Drawing.Size(728, 495);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private MetroSet_UI.Controls.MetroSetButton btnCheck;
+        private MetroSet_UI.Controls.MetroSetButton btnChooseFile;
+        private MetroSet_UI.Controls.MetroSetButton btnChooseDigitalSignature;
+        private MetroSet_UI.Controls.MetroSetTextBox tbxPlainTekst;
+        private MetroSet_UI.Controls.MetroSetLabel metroSetLabel3;
+        private MetroSet_UI.Controls.MetroSetLabel metroSetLabel1;
+    }
+}
